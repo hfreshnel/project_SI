6363b2159f72dec6c45ac01c71fef9cb1eeed2ed
Freshnel HOUENOU <fresh.kushimaru@gmail.com>
2025-01-26 19:03:02 +0100

    sign addind crypted hash

--- sign_message.py
+++ sign_message.py
@@ -1,14 +1,17 @@
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
+from hash_message import hash_message
 
 def sign_message(message, private_key, hash_function):
-    signature = private_key.sign(
-        message.encode(),
+    hashed_message = hash_message(message, hash_function)
+    signed_hash = private_key.sign(
+        hashed_message,
         padding.PSS(
             mgf=padding.MGF1(hash_function),
             salt_length=padding.PSS.MAX_LENGTH
         ),
         hash_function
     )
+    signed_message = message.encode() + signed_hash
     print("Message signed.")
-    return signature
+    return signed_message
